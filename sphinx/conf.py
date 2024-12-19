7dbadb6dcb020ec946f3d60ab250452025cf70c9
Luca Boccassi <luca.boccassi@gmail.com>
2024-12-19 17:54:22 +0000

    Fix build with new sphinx
    ERROR: Invalid value `None` in intersphinx_mapping['http://docs.python.org/']. Expected a two-element tuple or list.

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -308,4 +308,4 @@ epub_copyright = '2013-2014, Brian Trammell'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
